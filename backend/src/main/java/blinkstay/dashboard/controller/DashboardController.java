0e7c422320c98ed3c81b012ec76170f9b1f5662d
qutubuddinkhan07 <qutubuddinkhan8261@gmail.com>
2026-09-23 19:40:39 +0530

    corrected some bugs in dashboard api

--- backend/src/main/java/blinkstay/dashboard/controller/DashboardController.java
+++ backend/src/main/java/blinkstay/dashboard/controller/DashboardController.java
@@ -7,18 +7,24 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import blinkstay.dashboard.dto.ManagerDashboardDto;
 import blinkstay.dashboard.service.ManagerDashboardService;
 import blinkstay.listing.dto.ListingApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import lombok.RequiredArgsConstructor;
 
 @RestController
+@RequestMapping("/api/v5")
+@SecurityRequirement(name = "Bearer Authentication")
+@PreAuthorize("hasAuthority('HOTEL_MANAGER')")
+@RequiredArgsConstructor
 public class DashboardController {
-	private ManagerDashboardService managerDashboardService;
+	private final ManagerDashboardService managerDashboardService;
 
 	@GetMapping("/my-dashboard")
-	@PreAuthorize("hasAuthority('HOTEL_MANAGER')")
 	public ResponseEntity<ListingApiResponse<ManagerDashboardDto>> getManagerDashboard(
 			@AuthenticationPrincipal UserDetails userDetails) {
 
